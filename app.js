0c548e9a4359a8000338d9958915ccb0124b816a
Mike Java <mike.java.code@gmail.com>
2023-05-18 00:49:22 +0200

    sd

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
-var express = require('express');
+var express    = require('express');
 var bodyParser = require('body-parser');
-var pdf = require('html-pdf');
-var fs = require('fs');
-var options = { format: 'A4' };
-const util = require('util');
+var pdf        = require('html-pdf');
+var fs         = require('fs');
+var options    = {format:'A4'};
+
 //init app
 var app = express();
 
 //set the templat engine
-app.set('view engine', 'ejs');
+app.set('view engine','ejs');
 
 //fetch data from the request
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/', (req, res) => {
+app.get('/',(req,res)=>{
     res.render('home')
 });
 
-app.post('/', (req, res) => {
-
-    res.render('demopdf', { data: req.body.article }, function (err, html) {
-
-
-        const filePath = './demopdf.pdf';
-
-        pdf.create(html, options).toFile(filePath, (err, result) => {
-          if (err) {
-            console.error('PDF conversion error:', err);
-            res.status(400).json({
-              message: 'Error converting HTML to PDF'
-            });
-          } else {
-            console.log('PDF conversion completed:', result);
-      
-            const datafile = fs.readFileSync(filePath).toString('base64');
-            res.status(200).json({
-              message: datafile
-            });
-          }
-        });
+app.post('/',(req,res)=>{
+    res.render('demopdf',{data:req.body.article},function(err,html){
+        pdf.create(html, options).toFile('./public/uploads/demopdf.pdf', function(err, result) {
+            if (err){
+                return console.log(err);
+            }
+             else{
+            console.log(res);
+            var datafile = fs.readFileSync('./public/uploads/demopdf.pdf');
+            res.header('content-type','application/pdf');
+            res.send(datafile);
+             }
+          });
     })
 })
 
-
-
 //assign port
 var port = process.env.PORT || 3000;
-app.listen(port, () => console.log('server run at port ' + port));
+app.listen(port,()=>console.log('server run at port '+port));
 
